98d012e0fd47c941a3dc4f4e3419e104cfba50e4
programmersstudio <pulkitkhullar15@gmail.com>
2022-02-14 17:34:00 +0530

    initial commit

---
+++ packages/sample/hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+func Main(args map[string]interface{}) map[string]interface{} {
+	name, ok := args["name"].(string)
+	if !ok {
+		name = "stranger"
+	}
+	msg := make(map[string]interface{})
+	msg["body"] = "Hello " + name + "!"
+	return msg
+}
